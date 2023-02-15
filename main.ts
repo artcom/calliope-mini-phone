27aca526f0a1ed6d1174e603dd0d890ba9d93b1e
elRadish <andreas.rettig@artcom.de>
2023-02-15 17:49:06 +0100

    Cleanup and refactoring after review

--- main.ts
+++ main.ts
@@ -5,71 +5,65 @@ enum RadioMessage {
  * Pulse Input on P0
  */
 /**
- * Dial input on P1
+ * Dialing input on P1
  */
 /**
- * Fork on P3
+ * Telephone hook on P3
  */
 pins.onPulsed(DigitalPin.P0, PulseValue.Low, function () {
     if (control.millis() - lastPulse > 60) {
-        sendR("phone/onPulse " + pulseCount, 5)
-        pulseCount += 1
+        send("phone/onPulse " + pulses, 5)
         led.plot(0, 0)
-        if (counter < 9) {
-            counter += 1
+        if (pulses < 9) {
+            pulses += 1
         } else {
-            counter = 0
+            pulses = 0
         }
         lastPulse = control.millis()
     }
 })
-function sendR (message: string, repeat: number) {
+pins.onPulsed(DigitalPin.P1, PulseValue.High, function () {
+    if (control.millis() - lastDialstart > 300) {
+        send("phone/onStartDial", 5)
+        basic.clearScreen()
+        pulses = 0
+        led.plot(4, 0)
+        lastDialstart = control.millis()
+    }
+})
+pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
+    send("phone/onHang false ", 10)
+})
+function send (message: string, repeat: number) {
     serial.writeLine(message)
     for (let index = 0; index < repeat; index++) {
         radio.sendString(message)
     }
 }
-pins.onPulsed(DigitalPin.P1, PulseValue.High, function () {
-    if (control.millis() - lastDialstart > 300) {
-        sendR("phone/onStartDial", 5)
-        pulseCount = 0
-        basic.clearScreen()
-        counter = 0
-        led.plot(4, 0)
-        control.waitMicros(100000)
-        lastDialstart = control.millis()
-        lastPulse = control.millis()
-    }
-})
-pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
-    sendR("phone/onHang false ", 10)
-})
 pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        sendR("phone/onDialed " + counter, 40)
+        send("phone/onDialed " + pulses, 40)
         led.unplot(4, 0)
-        basic.showNumber(counter)
+        basic.showNumber(pulses)
     }
 })
 pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
     led.unplot(0, 0)
 })
 pins.onPulsed(DigitalPin.P3, PulseValue.High, function () {
-    sendR("phone/onHang true", 10)
+    send("phone/onHang true", 10)
 })
 let lastPulse = 0
 let lastDialstart = 0
-let pulseCount = 0
-let counter = 0
+let pulses = 0
 radio.setTransmitPower(7)
 radio.setGroup(1)
 radio.setFrequencyBand(70)
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
-basic.showNumber(counter)
-pulseCount = 0
+basic.showNumber(pulses)
 lastDialstart = 0
 loops.everyInterval(1200000, function () {
-    sendR("phone/vcc " + pins.analogReadPinInternalRef(), 1)
+    send("phone/vcc " + pins.analogReadPinInternalRef(), 1)
 })
